b9de9f30e0242b0287c03c3ccb845b6c4ac61286
MercyWanjau <mwanjau997@gmail.com>
2022-11-27 13:15:03 +0300

    phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'mercy'
+var bestCustomer;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'mercy'
+}
